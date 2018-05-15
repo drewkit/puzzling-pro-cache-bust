b2bf18498f05749b1f5df7ac2f300a75d3a5351b
Drew Kitch <drew.kitch@gmail.com>
2018-05-14 19:04:47 -0600

    bust cache

--- modify-foo.sh
+++ modify-foo.sh
@@ -4,8 +4,8 @@ set -e
 
 if [[ 'bust cache' == $CI_COMMIT_MESSAGE ]]
 then
-  echo 'altering foo.txt file? ---> YES'
+  echo 'modifying foo.txt file? ---> YES'
   echo $RANDOM >> foo.txt
 else
-  echo 'altering foo.txt file? ---> NO'
+  echo 'modifying foo.txt file? ---> NO'
 fi
